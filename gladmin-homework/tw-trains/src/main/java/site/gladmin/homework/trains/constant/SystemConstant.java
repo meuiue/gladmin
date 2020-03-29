7ad0400ed24f5276533bd1b4e622292afa220872
yitusite <sking123>
2020-03-29 14:51:41 +0800

    update gladmin dict

--- gladmin-homework/tw-trains/src/main/java/site/gladmin/homework/trains/constant/SystemConstant.java
+++
@@ -1,8 +0,0 @@
-package site.gladmin.homework.trains.constant;
-
-
-public final class SystemConstant {
-    /*输入文件路径*/
-    public static final String INPUT_FILE_PATH = "src/main/resources/map.txt";
-    public static final String NO_SUCH_ROUTE = "NO SUCH ROUTE";
-}
